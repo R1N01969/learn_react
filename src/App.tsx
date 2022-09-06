b0354a17abc7eb3de62e337535d399adddd38ea8
R1N0\hgms1 <hagamasa1@gmail.com>
2022-09-06 22:34:38 +0900

    入門編の内容追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react'
+import Child_Out_Of_File from './Child'
+import { Child1, Child2 } from './Children'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Item = {
+    id: number
+    title: string
 }
 
-export default App;
+type Props = {
+  number: string
+  children: React.ReactNode
+}
+
+const Child: React.FC<Props> = ({ number, children }) => {
+  return (
+    <p>{ number }の{ children }</p>
+  )
+}
+
+const App: React.FC = () => {
+  const [ count, setCount ] = useState<number>(0)
+
+  const handleIncrement = useCallback(() => {
+    setCount(prev => prev +1)
+  }, [])
+
+  const handleDecrement = useCallback(() => {
+    setCount(prev => prev -1)
+  }, []);
+  const greet: string = 'こんにちは React!!'
+  const items: Item[] = [
+    {
+      id: 1,
+      title: 'カップラーメン'
+    }, {
+      id: 2,
+      title: '冷凍食品'
+    }
+  ]
+  return (
+    <div className="App">
+      <div>
+        <button onClick={handleIncrement}>+1</button>
+        <button onClick={handleDecrement}>-1</button>
+        <p>{ count }</p>
+      </div>
+      <Child1 />
+      <Child2 />
+      <Child_Out_Of_File />
+      <Child number="1">子のコンポーネント</Child>
+      <Child number="2">子のコンポーネント</Child>
+      <ul>
+      { items.map((item: Item) => (
+          <li key={item.id}>{ item.title }</li>
+      )) }
+      </ul>
+      <p>{ greet }</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Child.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Child_Out_Of_File: React.FC = () => {
+	return (
+		<div>子のコンポーネント</div>
+	)
+}
+
+export default Child_Out_Of_File
